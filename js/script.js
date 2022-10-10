c6b3d5ee2ae77e0815cf2d06d2ccd54019fa4cb7
youldaw <68648932+youldaw@users.noreply.github.com>
2022-10-10 22:27:23 +0500

    fafsd
    sd;las;dl

--- js/script.js
+++ js/script.js
@@ -9,61 +9,80 @@ $(function (){
     
 
 
-    $(function () {
-        var swiper = new Swiper('.video-slide', {
-            loop: true,
-            slidesPerView: 1,
-            // spaceBetween: 15,
-            pagination: {
-                el: '.swiper-pagination',
-                type: 'fraction',
-                formatFractionCurrent: function (number) {
-                    return '0' + number;
-                }
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev'
+    var slider1 = new Swiper('.slider1', {
+        loop: true,
+        slidesPerView: 1,
+        // spaceBetween: 15,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+            formatFractionCurrent: function (number) {
+                return '0' + number;
             }
-        });
-
-        var swiper = new Swiper('.special-offers-slide', {
-            loop: true,
-            loopAdditionalSlides: 30,
-            slidesPerView: 3,
-            centeredSlides: true,
-            spaceBetween: 85,
-            pagination: {
-                el: '.swiper-pagination',
-                type: 'fraction',
-                formatFractionCurrent: function (number) {
-                    return '0' + number;
-                }
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev'
-            }
-        });
-
-        var swiper = new Swiper('.command-slide', {
-            loop: true,
-            slidesPerView: 4,
-            centeredSlides: true,
-            spaceBetween: 5,
-            pagination: {
-                el: '.swiper-pagination',
-                type: 'fraction',
-                formatFractionCurrent: function (number) {
-                    return '0' + number;
-                }
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev'
-            }
-        });
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
     });
+
+    var slider2 = new Swiper('.slider2', {
+        loop: true,
+        loopAdditionalSlides: 30,
+        slidesPerView: 3,
+        centeredSlides: true,
+        spaceBetween: 85,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+            formatFractionCurrent: function (number) {
+                return '0' + number;
+            }
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    });
+
+    var slider3 = new Swiper('.slider3', {
+        loop: true,
+        slidesPerView: 4,
+        // centeredSlides: true,
+        spaceBetween: 2,
+        realIndex: 4,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+            formatFractionCurrent: function (number) {
+                return '0' + number;
+            }
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    });
+
+    var mySwiper = new Swiper(".mySwiper", {
+        spaceBetween: 20,
+        slidesPerView: 3,
+        freeMode: true,
+        watchSlidesProgress: true,
+    });
+    var swiper2 = new Swiper(".mySwiper2", {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+            swiper: mySwiper,
+        },
+    });
+    galleryTop.controller.control = galleryThumbs;
+    galleryThumbs.controller.control = galleryTop;
+    
 
 
     
